92a4e6ec466722394beea8d0eab1fa65208169e8
ThxtToo <87293260+ThxtToo@users.noreply.github.com>
2024-06-22 14:28:09 -0400

    ahh nose
    testeando

--- main_syd.py
+++ main_syd.py
@@ -1,12 +1,13 @@
 import random
 
 
-def rock_paper_scissors():
+def rock_paper_or_scissors():
 	rps_list = ['"rock"', '"paper"', '"scissors"']
 	count = 0
 	infinite = True
-	print("---Welcome to the game of rock, paper, scissors.---\n")
+	print("\n---Welcome to the game of rock, paper, scissors.---\n")
 	print("Hello, my name is Syd and you will be playing against me.")
+	print("\nChoose rock, paper or scissors")
 
 	while infinite == True:
 		user = str(input("\n-----> "))
@@ -62,10 +63,50 @@ def rock_paper_scissors():
 
 			#exit for invalid input
 			else:
-				print("\nSea serio\n")
+				print("\nInvalid input.\n")
+
+#::==================================================================================================
+
+def heads_or_tails():
+	heads_or_tails_list = ['"heads"', '"tails"']
+
+	print("\n---Welcome to the game of heads or tails.---\n")
+	print("Hello, my name is Syd and you will be playing against me.")
+	print("Choose heads or tails")
+
+	while True:
+		user = str(input("\n-----> "))
+		for i in range(1):
+
+			if all(c in 'heads' for c in user):
+				syd = random.choice(heads_or_tails_list)
+
+				if syd == '"heads"':
+					print("\nHas fallen", syd, "--> You won")
+
+				elif syd == '"tails"':
+					print("\nHas fallen", syd, "--> Syd won")
+
+			elif all(c in 'tails' for c in user):
+				syd = random.choice(heads_or_tails_list)
+
+				if syd == '"tails"':
+					print("\nHas fallen", syd, "--> You won")
+
+				elif syd == '"heads"':
+					print("\nHas fallen", syd, "--> Syd won")
+
+			else:
+				print("Error")
 
 
-rock_paper_scissors()
 
+
+print("1 = Rock, Paper or Scissors\n2 = Heads or Tails")
+game = input("\nEscoge el juego: ")
+if game == "1":
+	rock_paper_or_scissors()
+elif game == "2":
+	heads_or_tails()
 
 
